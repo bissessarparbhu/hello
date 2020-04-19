8b617843ee6fdb03b396194c08f9310b56e67191
Parbhu Bissessar <bissessarparbhu@yahoo.com>
2020-04-19 11:40:42 -0400

    Updates

---
+++ precache-manifest.cd423f0f28a4da86e7b0d00c4a21806b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c36dd45a1b8bf55316e96804743513c",
+    "url": "/bissessarparbhu/hello/index.html"
+  },
+  {
+    "revision": "03c7c1bfd6d873c7b47e",
+    "url": "/bissessarparbhu/hello/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "9a8dc386918638825721",
+    "url": "/bissessarparbhu/hello/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "03c7c1bfd6d873c7b47e",
+    "url": "/bissessarparbhu/hello/static/js/2.d2036def.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/bissessarparbhu/hello/static/js/2.d2036def.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a8dc386918638825721",
+    "url": "/bissessarparbhu/hello/static/js/main.912f5b83.chunk.js"
+  },
+  {
+    "revision": "3210108b572f8281faff",
+    "url": "/bissessarparbhu/hello/static/js/runtime-main.b52270c2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/bissessarparbhu/hello/static/media/logo.5d5d9eef.svg"
+  }
+]);
